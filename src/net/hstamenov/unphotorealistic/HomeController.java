090755d189b2412bc0a83552db77dba8f118dad6
hstamenov <hstamenov@DEVSPS2013I02.ebs.internal>
2018-01-09 13:31:02 +0200

    Configured window with top menu.

--- src/net/hstamenov/unphotorealistic/HomeController.java
+++ src/net/hstamenov/unphotorealistic/HomeController.java
@@ -5,9 +5,14 @@
  */
 package net.hstamenov.unphotorealistic;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
 
 /**
  * FXML Controller class
@@ -15,13 +20,20 @@ import javafx.fxml.Initializable;
  * @author hstamenov
  */
 public class HomeController implements Initializable {
-
+    @FXML
+    private BorderPane root;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
     
+    public void openFile() {
+        FileChooser fileChooser = new FileChooser();
+        //FileChooser.ExtensionFilter extensionFilter = new FileChooser.ExtensionFilter("Images", "*.png");
+        File file = fileChooser.showOpenDialog(root.getScene().getWindow());
+        System.out.println(file.getAbsolutePath());
+    }
 }
